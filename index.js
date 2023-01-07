ac1b957bea08c96d92a87d70e6a06ee7f431f9b7
eslavi <eslavi_jonhas@hotmail.com>
2023-01-06 22:51:51 -0600

    Cities search and env variables now works

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express from 'express'
+
 import {
-    inquirerMenu, pausa, leerInput, listadoTareasBorrar, confirmar, mostrarListadoChecklist
+    inquirerMenu, pausa, leerInput
 } from './helpers/inquirer.js';
+import { Busquedas } from './models/busquedas.js';
+
+dotenv.config();
 
 const main = async() => {
     let opt = 0;
+    const busquedas = new Busquedas();
     do{
         opt = await inquirerMenu();
 
         switch(opt){
             case 1: //Escribir ciudad
                 const lugar = await leerInput('Ciudad: ');
-                console.log(lugar);
+                await busquedas.ciudad(lugar);
                 
                 //Buscar los lugares
                 //Seleccionar el lugar
                 //Clima
                 //Mostrar resultados
-                console.log(`\nInformacion de la ciudad\n`.brightMagenta);
+                console.log(`\nInformacion de la ciudad\n`.cyan);
                 console.log(`Ciudad:`, );
                 console.log(`Lat:`, );
                 console.log(`Lng:`, );
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,14 +1,38 @@
+import axios from 'axios';
+import { pausa } from '../helpers/inquirer.js';
+
 export class Busquedas {
-    historial = ['Chapala','Tonala','Texas'];
+    historial = ['Houston','Grecia','Texas'];
 
     constructor() {
         //TODO: Leer db si existe
     }
 
-    async ciudad(lugar = '') {
-        //TODO: peticion http del lugar
-        console.log(lugar);
+    get paramsMapBox() {
+        return {
+            'access_token': process.env.MAPBOX_KEY,
+            'language': 'es,en',
+            'limit': 5
+        }
+    }
 
-        return [] //lugares que pidio
+    async ciudad(lugar = '') {
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                timeout: 2000,
+                params: this.paramsMapBox
+            });
+
+            const resp = await instance.get();
+            console.log(resp.data);
+
+            return [];
+
+        } catch(error) {
+            return [];
+        }
+        
+        
     }
 }
